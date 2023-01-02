31b65ae96b53a302bba1ccc479364d4cfe3101bd
nlct <n.talbot@uea.ac.uk>
2023-01-02 16:54:53 +0000

    updated version number

--- pass-checker/java/PassChecker.java
+++ pass-checker/java/PassChecker.java
@@ -1175,7 +1175,7 @@ public class PassChecker extends Vector<AssignmentMetaData>
 
    public static final String NAME="PASS Checker";
    public static final String INVOKER_NAME="pass-checker";
-   public static final String VERSION="1.4";
+   public static final String VERSION="1.4.1";
    public static final String VERSION_DATE="2022-12-05";
    public static final int COPYRIGHT_START_YEAR=2018;
 
